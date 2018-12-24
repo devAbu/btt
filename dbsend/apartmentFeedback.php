3a9e8d6488cc388296f30aa9d6ea952f634c9d0e
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2018-12-24 19:31:42 +0100

    mmm

--- dbsend/apartmentFeedback.php
+++ dbsend/apartmentFeedback.php
@@ -1,22 +1,20 @@
 <?php
 
-    require "../connection/connect.php";
+require "../connection/connect.php";
 
-    $id = $_REQUEST["idRes"];
-    $feedback = $_REQUEST["feedback"];
-    $session = $_REQUEST["session"];
+$id = $_REQUEST["idRes"];
+$feedback = $_REQUEST["feedback"];
+$session = $_REQUEST["session"];
 
-    if($_REQUEST["task"] == "sendFeed"){
-        $sql = "INSERT INTO apartmentFeedback (`name`, `apartmentID`, `feedback`) VALUES ('$session', $id, '$feedback')";
+if ($_REQUEST["task"] == "sendFeed") {
+    $sql = "INSERT INTO apartmentFeedback (`name`, `apartmentID`, `feedback`) VALUES ('$session', $id, '$feedback')";
 
-        $response = @mysqli_query($dbc, $sql);
-        if ($response) {
-            echo ('sent');
-        } else {
-            echo mysqli_error($dbc);
-        }
+    $response = @mysqli_query($dbc, $sql);
+    if ($response) {
+        echo ('sent');
+    } else {
+        echo mysqli_error($dbc);
     }
+}
 
 ?>
-
-ff
